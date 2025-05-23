e889a83190485d092ba3038a495d289d15b5ee3f
최정민 <71782230+jeongmin521@users.noreply.github.com>
2025-05-23 09:16:40 +0900

    Update and rename 개념.txt to 개념.cpp

--- c++/map/개념.cpp
+++ c++/map/개념.cpp
@@ -1,11 +1,13 @@
+/*
 map
 키-값 쌍을 저장하는 컨테이너로, 키를 기준으로 자동 정렬
  - 키는 유일해야 함
  - 자동 정렬 (기본: 오름차순)
  - 탐색, 삽입, 삭제: O(log N) (RB-Tree 기반)
  - 임의 접근 불가
+ */
 
-1. 접근
+//1. 접근
 #include <iostream>
 #include <map>
 using namespace std;
@@ -27,14 +29,15 @@ int main() {
     return 0;
 }
 
-// 출력결과
+/* 출력결과
 Alice: 95
 Bob: 80
 Charlie: 85
 Bob의 점수: 80
+ */
 
 
-2. 삽입/삭제
+//2. 삽입/삭제
 #include <iostream>
 #include <map>
 using namespace std;
@@ -66,12 +69,34 @@ int main() {
     return 0;
 }
 
-//출력결과
+/*출력결과
 apple: 3
 grape: 5
 orange: 7
 grape 개수: 5
 grape 없음
+*/
+
+//3. iterator 사용
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+   map<string, int> m = {{"apple", 1}, {"banana", 2}};
+    for (map<string, int>::iterator it = m.begin(); it != m.end(); ++it){
+        cout << it->first << ": " << it->second << endl;
+    }
+}
+
+/*
+apple: 1
+banana: 2
+*/
 
 
+//iterator는 auto로 간단하게 쓸 수 있음
+for (auto it = m.begin(); it != m.end(); ++it) {
+    cout << it->first << ": " << it->second << endl;
+}
 
