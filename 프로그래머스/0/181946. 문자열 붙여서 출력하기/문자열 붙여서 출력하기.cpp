fd444b0a369ae3fa5888d8d28081fac51fde2ba7
최정민 <71782230+jeongmin521@users.noreply.github.com>
2025-06-02 17:41:08 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 2.55 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str1, str2;
+    cin >> str1 >> str2;
+    
+    cout << str1 << str2;
+    return 0;
+}
+
+/*
+공백으로 구분되어 입력된 문자열 합치기
+공백으로 구분해서 각각 저장하고 출력
+*/
