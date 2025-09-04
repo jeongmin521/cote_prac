a5e17931414780bf3f3f21b585045f2e75c8f8c1
최정민 <71782230+jeongmin521@users.noreply.github.com>
2025-09-05 07:47:49 +0900

    [level 0] Title: 배열 만들기 6, Time: 8.39 ms, Memory: 32.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> answer;
+    for(int i = 0; i < arr.size(); i++){
+        if(answer.empty()){
+            answer.push_back(arr[i]);
+        }
+        else if(answer.back() == arr[i]){
+            answer.pop_back();
+        }
+        else if(answer.back() != arr[i]){
+            answer.push_back(arr[i]);
+        }
+    }
+    if(answer.empty()){
+            answer.push_back(-1);
+        }
+    return answer;
+}
