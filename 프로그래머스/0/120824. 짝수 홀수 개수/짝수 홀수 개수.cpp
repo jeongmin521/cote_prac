4433b4e76694c1ec8dbd9c44d6b42364eb70e98d
최정민 <71782230+jeongmin521@users.noreply.github.com>
2025-12-02 07:57:27 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 3.61 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer;
+    int x = 0;
+    int y = 0;
+    for(int i = 0; i < num_list.size(); i++){
+        if(num_list[i] % 2 == 0){
+            x += 1;
+        }
+        else {
+            y += 1;
+        }
+    }
+    answer.push_back(x);
+    answer.push_back(y);
+    return answer;
+}
