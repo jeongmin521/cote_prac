dfa1cb3d53d91ea9a346daf400b4248374e628cc
최정민 <71782230+jeongmin521@users.noreply.github.com>
2026-01-19 08:15:49 +0900

    [level 0] Title: 369게임, Time: 0.02 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int order) {
+    int answer = 0;
+    string temp = to_string(order);
+    for(int i = 0; i < temp.length(); i++){
+        if (temp[i] == '3' || temp[i] == '6' || temp[i] == '9'){
+            answer += 1;
+        }
+    }
+    return answer;
+}
